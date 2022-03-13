5f3d72a86cc3a9a2545b4663552a9f7337c2e06d
Josu Vazquez <josu9513@gmail.com>
2022-03-13 20:17:06 +0100

    Import enviroment

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,4 +1,6 @@
+var environment = require('./src/environments/environment');
 importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js");
-firebase.initializeApp(environoment.firebaseConfig);
+
+firebase.initializeApp(environment.firebaseConfig);
 var messaging = firebase.messaging();

af7a81eac537bda1f5a492b98ff2f3974ebcec50
Josu Vazquez <josu9513@gmail.com>
2022-03-13 20:43:40 +0100

    Fix serviceworker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,12 +30,6 @@ initializeApp(environment.firebaseConfig);
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       registrationStrategy: 'registerWhenStable:30000'
-    }),
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
     })
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
